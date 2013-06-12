e98555b430680bcd7dd56461551f9d0247289710
Univer Shi <univer.shi@gmail.com>
2013-06-13 00:43:59 +0800

    Update settings

--- src/genghis/settings.py
+++ src/genghis/settings.py
@@ -54,7 +54,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(GENGHIS_BASE_DIR, os.pardir, 'uploaded')
+MEDIA_ROOT = os.environ.get('GENGHIS_MEDIA_ROOT', os.path.join(GENGHIS_BASE_DIR, os.pardir, 'uploaded'))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
