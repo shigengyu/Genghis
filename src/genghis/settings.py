be006594cb35d7a7f6533b917651a8a5d73b9cad
Univer Shi <univer.shi@gmail.com>
2013-06-13 00:30:50 +0800

    update settings

--- src/genghis/settings.py
+++ src/genghis/settings.py
@@ -65,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/shigengy/public_html'
+STATIC_ROOT = '/home/shigengy/public_html/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -77,7 +77,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(GENGHIS_BASE_DIR, 'static'),
-    STATIC_ROOT,
 )
 
 # List of finder classes that know how to find static files in
