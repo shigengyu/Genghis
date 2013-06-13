34e2855d55f5b57e8205245d58e4d1808e0ca0b3
Univer Shi <univer.shi@gmail.com>
2013-06-13 22:21:22 +0800

    Minor change

--- src/home/authentication.py
+++ src/home/authentication.py
@@ -30,4 +30,6 @@ class RequireAdmin(RequireLogin):
         return result
 
 def is_admin(user):
-    return user.is_authenticated and (user.is_superuser or GENGHIS_ENVIRONMENT == 'dev')
+    authenticated = user.is_authenticated()
+    is_superuser = user.is_superuser or GENGHIS_ENVIRONMENT == 'dev'
+    return authenticated and is_superuser
