25cfc5fd24ee67d6911a7518203af60da435816f
Univer Shi <univer.shi@gmail.com>
2013-06-15 18:51:11 +0800

    Fix static dir settings

--- src/genghis/settings.py
+++ src/genghis/settings.py
@@ -79,10 +79,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(GENGHIS_BASE_DIR, 'static', 'font'),
-    os.path.join(GENGHIS_BASE_DIR, 'static', 'img'),
-    os.path.join(GENGHIS_BASE_DIR, 'static', 'js'),
-    os.path.join(GENGHIS_BASE_DIR, 'static', 'style'),
+    os.path.join(GENGHIS_BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
