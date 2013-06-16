d1bbc5ade2f88caa923733ee030c232d834bc337
Univer Shi <univer.shi@gmail.com>
2013-06-16 15:17:17 +0800

    Support search by tag for photos.

--- src/photos/urls.py
+++ src/photos/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
-from photos.views import PhotoList, PhotoGallery, PhotoUpload, PhotoUpdate, PhotoDelete
+from photos.views import PhotoList, PhotoGallery, PhotoGalleryByTag, PhotoUpload, PhotoUpdate, PhotoDelete
 from photos.views import PhotoTagList, PhotoTagCreate, PhotoTagUpdate, PhotoTagDelete
 
 urlpatterns = patterns('',
     url(r'^$', PhotoList.as_view()),
     url(r'^gallery/$', PhotoGallery.as_view()),
+    url(r'^gallery/bytag/(?P<slug>[-\w]+)/$', PhotoGalleryByTag.as_view()),
     url(r'^upload/$', PhotoUpload.as_view()),
     url(r'^update/(?P<pk>\d+)/$', PhotoUpdate.as_view()),
     url(r'^delete/(?P<pk>\d+)/$', PhotoDelete.as_view()),
