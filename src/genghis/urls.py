db333eade3da3897ef826e3451f5ffa50f2482d6
Univer Shi <univer.shi@gmail.com>
2013-06-17 23:36:40 +0800

    Fix php page redirect

--- src/genghis/urls.py
+++ src/genghis/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url, static
 from django.conf import settings
-
-from django.contrib import admin
-admin.autodiscover()
+from home.views import WikiRedirectView, MySqlRedirectView
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', include('home.urls')),
@@ -13,10 +13,12 @@ urlpatterns = patterns('',
     url(r'^about/', include('about.urls')),
     url(r'^profile/', include('about.urls')),
     url(r'^socialauth/', include('social_auth.urls')),
+    url(r'^wiki/', WikiRedirectView.as_view()),
+    url(r'^mysql/', MySqlRedirectView.as_view()),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
--- src/home/urls.py
+++ src/home/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import patterns, url
-from home.views import HomeView, LoggedInView, LoginView, LogoutView,\
-    WikiRedirectView, MySqlRedirectView
+from home.views import HomeView, LoggedInView, LoginView, LogoutView
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view()),
     url(r'^logged-in/', LoggedInView.as_view()),
     url(r'^login/', LoginView.as_view()),
     url(r'^logout/', LogoutView.as_view()),
-    url(r'^wiki/', WikiRedirectView.as_view()),
-    url(r'^mysql/', MySqlRedirectView.as_view()),
 )
