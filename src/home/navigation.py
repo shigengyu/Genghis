6d55044b3d23727a4ad0cc9f1be59b40c86c8646
Univer Shi <univer.shi@gmail.com>
2013-06-22 14:11:29 +0800

    Fix syntax error

--- src/home/navigation.py
+++ src/home/navigation.py
@@ -1,14 +1,3 @@
 def generate_navigation(request):
     url = request.get_full_path()
     return { 'url': url }
-
-def generate_path_items(url):
-    elements = url.split('/')
-    path_items = []
-    for e in elements:
-        pass
-
-url_name_mapping = {
-                    'articles', 'Articles'
-                    'tags', 'Tags'
-                    }
