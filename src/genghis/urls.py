710da30f2191209cd3eec0a2c1fdd61b687bdde1
shigengyu <univer.shi@gmail.com>
2014-04-11 21:48:38 +0100

    Test

--- src/genghis/urls.py
+++ src/genghis/urls.py
@@ -12,8 +12,8 @@ urlpatterns = patterns('',
     url(r'^profile/', include('about.urls')),
     url(r'^socialauth/', include('social_auth.urls')),
     url(r'^wiki/', WikiRedirectView.as_view()),
+    url(r'^wp/', WordPressRedirectView.as_view()),
     url(r'^mysql/', MySqlRedirectView.as_view()),
-    url(r'^wordpress/', WordPressRedirectView.as_view()),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- src/genghis/views.py
+++ src/genghis/views.py
@@ -4,10 +4,10 @@ class WikiRedirectView(RedirectView):
     def get_redirect_url(self, **kwargs):
         return '/wiki/doku.php'
 
-class MySqlRedirectView(RedirectView):
-    def get_redirect_url(self, **kwargs):
-        return '/mysql/index.php'
-
 class WordPressRedirectView(RedirectView):
     def get_redirect_url(self, **kwargs):
         return '/wp/index.php'
+
+class MySqlRedirectView(RedirectView):
+    def get_redirect_url(self, **kwargs):
+        return '/mysql/index.php'
